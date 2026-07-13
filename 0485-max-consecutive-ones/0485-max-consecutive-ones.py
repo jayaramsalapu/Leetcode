e68ee01be1c5c90076accc5935859d9b934a6821
Jayaram Salapu <jayaram.salapu28@gmail.com>
2026-07-13 11:07:57 +0530

    Time: 16 ms (81.64%), Space: 13.4 MB (93.49%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.py
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.py
@@ -8,7 +8,8 @@ class Solution(object):
         for i in nums:
             if i == 1:
                 count+=1
-                max_count = max(max_count,count)
+                if count> max_count:
+                    max_count = count
             else:
                 count = 0
         return max_count
