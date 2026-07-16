baf3f366154b690d8ae620304a9a4f8b36e822d9
Jayaram Salapu <jayaram.salapu28@gmail.com>
2026-07-16 13:28:58 +0530

    Time: 6 ms (28.65%), Space: 12.4 MB (17.76%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -1,8 +1,19 @@
 class Solution(object):
     def mySqrt(self, x):
-        """
-        :type x: int
-        :rtype: int
-        """
-        return int(x**0.5)
-        
+        if x < 2:
+            return x
+
+        left = 1
+        right = x // 2
+        ans = 0
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            if mid <= x // mid:
+                ans = mid
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return ans
